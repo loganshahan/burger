43d50d12e5592f419c1a439288aee1cb70f0ec53
loganshahan <loganshahan88@gmail.com>
2019-03-27 11:15:38 -0400

    all buttons work

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -29,9 +29,9 @@ router.put("/api/burgers/:id", function(req, res) {
 
   console.log("condition", condition);
 
-  burger.update(
+  burger.updateOne(
     {
-      devoured: req.body.devoured
+      devoured: true
     },
     condition,
     function(result) {
